91b39b9e2cfb867f7145f253d399c688f8f46c61
Yega Cho <yega1009@gmail.com>
2023-09-03 23:32:07 -0400

    created questions for user input

--- index.js
+++ index.js
@@ -3,13 +3,60 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'project title',
+        message: 'Please enter the project title',
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide the description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide the installation instruction',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information',
+    },
+    {
+        type: 'input',
+        name: 'contribution guidelines',
+        message: 'Please provide contribution guidelines',
+    },
+    {
+        type: 'input',
+        name: 'test instructions',
+        message: 'Please provide test instructions',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license',
+        options: ['MIT', 'Apache', 'GPL', 'BSD', 'MPL', 'EPL', 'Creative Commons']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide github username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide email address',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
