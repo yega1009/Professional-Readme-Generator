7882862a9a7ff957861c89504b7cd81b559c5cbd
Yega Cho <yega1009@gmail.com>
2023-09-03 23:40:58 -0400

    created function to initialize app

--- index.js
+++ index.js
@@ -54,13 +54,19 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFileSync(filename, data, (err) => {
+    fs.writeFileSync(fileName, data, (err) => {
         err ? console.log(err) : console.log('README file written.');
     });
  }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile('README.md', readmeContent);
+    })
+ }
 
 // Function call to initialize app
 init();
