e598559064c7e3dabb666e61e8c692a692adaada
Yega Cho <yega1009@gmail.com>
2023-09-03 23:34:21 -0400

    created functino to write readme file

--- index.js
+++ index.js
@@ -53,7 +53,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFileSync(filename, data, (err) => {
+        err ? console.log(err) : console.log('README file written.');
+    });
+ }
 
 // TODO: Create a function to initialize app
 function init() { }
